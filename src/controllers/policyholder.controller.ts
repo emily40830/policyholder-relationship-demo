8ed32b11c1fdb7483c9685d4ff086319ba37ac24
emily40830 <emily40830@gmail.com>
2023-12-12 17:36:21 +0800

    feat: modify api logic

--- src/controllers/policyholder.controller.ts
+++ src/controllers/policyholder.controller.ts
@@ -1,43 +1,72 @@
 import { RequestHandler } from "express";
 import { PrismaClient } from "@prisma/client";
-import { object, string, number } from "yup";
 import { isEmpty } from "ramda";
 
 const prismaClient = new PrismaClient();
 
+type PolicyHolder = {
+  code: string;
+  name: string;
+  depth: number;
+  registration_date: Date;
+  introducer_code: string | null;
+};
+
+function padDigits(number: number, digits: number): string {
+  return (
+    Array(Math.max(digits - String(number).length + 1, 0)).join("0") + number
+  );
+}
+
 class PolicyHolderController {
   public static getPolicyHolder: RequestHandler = async (req, res) => {
-    let targetCode = 1;
-    const querySchema = object({
-      code: number().integer().optional().default(1),
-    });
-    try {
-      await querySchema.validate(req.query);
-    } catch (error) {
+    const targetCode = Number(req.query.code);
+
+    if (isNaN(targetCode)) {
       res.status(400).send({
-        message: (error as Error).message,
+        message: "invalid code",
         result: null,
       });
       return;
     }
-    const { code } = await querySchema.cast(req.query);
-    targetCode = code;
 
     try {
-      const policyholder = await prismaClient.policyholder.findUnique({
+      const policyholderResp = await prismaClient.policyholder.findUnique({
         where: { code: targetCode },
       });
 
-      if (isEmpty(policyholder) || policyholder === null) {
+      if (isEmpty(policyholderResp) || policyholderResp === null) {
         res.status(404).send({
-          message: `${code} doesn't exist`,
+          message: `${targetCode} doesn't exist`,
           result: null,
         });
         return;
       }
+
+      const leftSide = await PolicyHolderController.getChildernPolicyHolders(
+        policyholderResp.leftIntroducedCode,
+        policyholderResp.depth,
+        4
+      );
+      const rightSide = await PolicyHolderController.getChildernPolicyHolders(
+        policyholderResp.rightIntroducedCode,
+        policyholderResp.depth,
+        4
+      );
+
+      const newPolicyHolder: PolicyHolder = {
+        code: padDigits(policyholderResp.code, 10),
+        name: policyholderResp.name,
+        depth: policyholderResp.depth,
+        registration_date: policyholderResp.registrationDate,
+        introducer_code: policyholderResp.introducerCode
+          ? padDigits(policyholderResp.introducerCode, 10)
+          : null,
+      };
+
       res.status(200).send({
         message: "successfully get policyholder",
-        result: policyholder,
+        result: { ...newPolicyHolder, l: leftSide, r: rightSide },
       });
     } catch (error) {
       res.status(500).send({
@@ -49,22 +78,16 @@ class PolicyHolderController {
   };
 
   public static getTopPolicyHolderByCode: RequestHandler = async (req, res) => {
-    // validate input
-    let targetCode = 1;
-    const paramsSchema = object({
-      code: number().integer().required(),
-    });
-    try {
-      await paramsSchema.validate(req.params);
-    } catch (error) {
+    const code = req.params.code;
+    const targetCode = Number(code);
+
+    if (isNaN(targetCode)) {
       res.status(400).send({
-        message: (error as Error).message,
+        message: "invalid code",
         result: null,
       });
       return;
     }
-    const { code } = await paramsSchema.cast(req.params);
-    targetCode = code;
 
     try {
       const policyholder = await prismaClient.policyholder.findUnique({
@@ -73,14 +96,58 @@ class PolicyHolderController {
 
       if (isEmpty(policyholder) || policyholder === null) {
         res.status(404).send({
-          message: `${code} doesn't exist`,
+          message: `${targetCode} doesn't exist`,
           result: null,
         });
         return;
       }
+      if (!policyholder.parentIntroducerCode) {
+        res.status(404).send({
+          message: `${targetCode} is the top of full structure`,
+          result: null,
+        });
+        return;
+      }
+
+      const parentPolicyHolder = await prismaClient.policyholder.findUnique({
+        where: { code: policyholder.parentIntroducerCode },
+      });
+
+      if (isEmpty(parentPolicyHolder) || parentPolicyHolder === null) {
+        res.status(404).send({
+          message: `top PolicyHolder ${padDigits(
+            policyholder.parentIntroducerCode,
+            10
+          )} doesn't exist`,
+          result: null,
+        });
+        return;
+      }
+
+      const leftSide = await PolicyHolderController.getChildernPolicyHolders(
+        parentPolicyHolder.leftIntroducedCode,
+        parentPolicyHolder.depth,
+        4
+      );
+      const rightSide = await PolicyHolderController.getChildernPolicyHolders(
+        parentPolicyHolder.rightIntroducedCode,
+        parentPolicyHolder.depth,
+        4
+      );
+
+      const newParentPolicyHolder: PolicyHolder = {
+        code: padDigits(parentPolicyHolder.code, 10),
+        name: parentPolicyHolder.name,
+        depth: parentPolicyHolder.depth,
+        registration_date: parentPolicyHolder.registrationDate,
+        introducer_code: parentPolicyHolder.introducerCode
+          ? padDigits(parentPolicyHolder.introducerCode, 10)
+          : null,
+      };
+
       res.status(200).send({
         message: `successfully get top from policyholder ${code}`,
-        result: policyholder,
+        result: { ...newParentPolicyHolder, l: leftSide, r: rightSide },
       });
     } catch (error) {
       res.status(500).send({
@@ -89,6 +156,48 @@ class PolicyHolderController {
       });
       return;
     }
+  };
+
+  private static getChildernPolicyHolders: (
+    code: number | null,
+    depth: number,
+    maxDepth: number
+  ) => Promise<PolicyHolder[]> = async (code, depth, maxDepth = 4) => {
+    if (!code) {
+      return [];
+    }
+    const result: PolicyHolder[] = [];
+    const queue: number[] = [];
+    queue.push(code);
+    let currentDepth = depth;
+    const targetDepth = maxDepth + depth - 1;
+    while (queue.length > 0 && currentDepth <= targetDepth) {
+      const targetCode = queue.shift();
+      const targetResp = await prismaClient.policyholder.findUnique({
+        where: { code: targetCode },
+      });
+      if (!targetResp) {
+        continue;
+      }
+      const newTarget: PolicyHolder = {
+        code: padDigits(targetResp.code, 10),
+        name: targetResp.name,
+        depth: targetResp.depth,
+        registration_date: targetResp.registrationDate,
+        introducer_code: targetResp.introducerCode
+          ? padDigits(targetResp.introducerCode, 10)
+          : null,
+      };
+      result.push(newTarget);
+      if (targetResp?.leftIntroducedCode) {
+        queue.push(targetResp?.leftIntroducedCode);
+      }
+      if (targetResp?.rightIntroducedCode) {
+        queue.push(targetResp?.rightIntroducedCode);
+      }
+      currentDepth += 1;
+    }
+    return result;
   };
 }
 
